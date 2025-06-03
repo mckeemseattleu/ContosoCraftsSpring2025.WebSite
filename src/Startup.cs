69b9835243ae5d6910884fedef7e861a0fe11e02
mckeemseattleu <mckeem@seattleu.edu>
2025-06-02 19:39:00 -0700

    I added a comment to Startup.cs file

--- src/Startup.cs
+++ src/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 // We are adding a comment
+// I'm adding another comment
 namespace ContosoCrafts.WebSite
 {
     public class Startup
